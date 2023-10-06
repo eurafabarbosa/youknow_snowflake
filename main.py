d4fe29d6e9fe09951f8a1485d7d4ac9fa90990ba
Baurpas <pascal.baur@snowflake.com>
2023-10-06 21:57:39 +0200

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-from snowflake.snowpark import Session
+#from snowflake.snowpark import Session
 
 
 # Everything is accessible via the st.secrets dict:
@@ -12,3 +12,14 @@ st.write("account name:", st.secrets["account"])
 st.title("YouKnow_X: Your daily digest :hamburger:")
 
 
+st.set_page_config(
+    page_title='YouKnow_X',
+    page_icon=':arrow_forward:',
+    layout='wide',
+    menu_items={
+    'Get Help': 'https://www.google.com/',
+    'Report a bug': 'https://www.google.com/',
+    'About': 'Streamlit Podcast App'
+
+    },
+)
