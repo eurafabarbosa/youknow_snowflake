57601fd4fa1bec3ca1822dc15fba7f245dddc9b0
Baurpas <pascal.baur@snowflake.com>
2023-10-06 21:40:26 +0200

    test secrets

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+from snowflake.snowpark import Session
+from resources.snow_config import snowflake_conn
+
+
+# Everything is accessible via the st.secrets dict:
+st.write("account name:", st.secrets["account"])
+
+
 
 st.title("YouKnow_X: Your daily digest :hamburger:")
+
+
