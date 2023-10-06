22a184ace71f8fd65efc190d8e77c6779fc1286b
Baurpas <pascal.baur@snowflake.com>
2023-10-06 21:58:38 +0200

    update

--- main.py
+++ main.py
@@ -4,14 +4,6 @@ import streamlit as st
 #from snowflake.snowpark import Session
 
 
-# Everything is accessible via the st.secrets dict:
-st.write("account name:", st.secrets["account"])
-
-
-
-st.title("YouKnow_X: Your daily digest :hamburger:")
-
-
 st.set_page_config(
     page_title='YouKnow_X',
     page_icon=':arrow_forward:',
@@ -23,3 +15,11 @@ st.set_page_config(
 
     },
 )
+
+# Everything is accessible via the st.secrets dict:
+st.write("account name:", st.secrets["account"])
+
+
+st.title("YouKnow_X: Your daily digest :hamburger:")
+
+
