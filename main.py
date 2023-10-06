ae82f05db11dc1cc12e3f4d1169991fea12a64cc
Baurpas <pascal.baur@snowflake.com>
2023-10-06 22:01:46 +0200

    update

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-#from snowflake.snowpark import Session
+import pinecone
+
+from snowflake.snowpark import Session
 
 
 st.set_page_config(
@@ -17,8 +19,6 @@ st.set_page_config(
 )
 
 # Everything is accessible via the st.secrets dict:
-st.write("account name:", st.secrets["account"])
-
 
 st.title("YouKnow_X: Your daily digest :hamburger:")
 
