2c96058127f2abda7c98fced98101397d6fca785
Baurpas <pascal.baur@snowflake.com>
2023-10-07 11:07:13 +0200

    update

--- main.py
+++ main.py
@@ -67,22 +67,41 @@ filter = st.selectbox(
 
 cs.execute(f"SELECT * FROM  VIDEOS WHERE AUTHOR LIKE '{filter}' order by PUB_DATE_MS DESC LIMIT 3")
 snow_df = cs.fetch_pandas_all()
-st.write(snow_df)
 
 query = st.text_input('Ask a question about Snowflake', '', key="vid_search")
 st.write(query)
 if query:
-    st.write(query)
     xq = model.encode(query).tolist()
     response = index.query(xq, top_k=3, include_metadata=True)
     start = response['matches'][0]['metadata']['start']
     url = response['matches'][0]['metadata']['title']+'&t='+str(start)+'s'
     st_player(url, key="question_player")
-    st.write(url)
+#    st.write(url)
 #    st.write(response['matches'])
 
 
 row1_col1, row1_col2, row1_col3= st.columns((3,3,3))
 
 with row1_col1:
-    st_player(snow_df.VID_URL.iloc[0], key="col1a_player")
+    st_player(snow_df.VID_URL.iloc[1], key="col1a_player")
+    expander = st.expander(":robot_face: See summary")
+    expander.write(
+        snow_df.SUMMARY.iloc[1]
+    )
+
+
+with row1_col2:
+    st_player(snow_df.VID_URL.iloc[2], key="col1a_player")
+    expander = st.expander(":robot_face: See summary")
+    expander.write(
+        snow_df.SUMMARY.iloc[2]
+    )
+
+
+
+with row1_col3:
+    st_player(snow_df.VID_URL.iloc[3], key="col1a_player")
+    expander = st.expander(":robot_face: See summary")
+    expander.write(
+        snow_df.SUMMARY.iloc[3]
+    )
