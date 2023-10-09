9a8e0f2775166d6c36ba91445290dfab3c38d2f5
Baurpas <pascal.baur@snowflake.com>
2023-10-09 15:31:06 +0200

    update

--- main.py
+++ main.py
@@ -103,18 +103,18 @@ if query:
     st.write(context)
     prompt = f"Q: Answer the following question {query} in the third person form, limit your answer to two sentences and base your answer on the following context: {context} Answer:"
     #with st.spinner('Thinking...'):
-   #res = openai.Completion.create(
-                #engine='text-davinci-003',
-                #prompt=prompt,
-                #temperature=0,
-                #max_tokens=400,
-                #top_p=1,
-                #frequency_penalty=0,
-                #presence_penalty=0,
-                #stop=None
-            #)
+    res = openai.Completion.create(
+                engine='text-davinci-003',
+                prompt=prompt,
+                temperature=0,
+                max_tokens=400,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0,
+                stop=None
+            )
             #st.success('Here is your answer!')
-    #res['choices'][0]['text'].strip()
+    st.write(res['choices'][0]['text'].strip())
             #t = st.empty()
             #for i in range(len(res['choices'][0]['text'].strip()) + 1):
             #t.markdown("## %s..." % output[0:i])
