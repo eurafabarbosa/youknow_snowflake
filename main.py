ff861f7c2411946654cccaef63877b66716384cd
Baurpas <pascal.baur@snowflake.com>
2023-11-08 00:17:15 +0100

    update

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ import snowflake.connector
 
 
 st.set_page_config(
-    page_title='YouKnow_X',
+    page_title='YouKnowSnow',
     page_icon=':arrow_forward:',
     layout='wide',
     menu_items={
     'Get Help': 'https://www.google.com/',
     'Report a bug': 'https://www.google.com/',
-    'About': 'Streamlit Podcast App'
+    'About': 'Streamlit App to find answers about the Snowflake Data Cloud'
 
     },
 )
