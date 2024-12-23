8675715987c88897ade9a99166ee99ec772d97a9
doyley91 <baurpas@gmail.com>
2024-12-23 22:45:35 +0100

    Update main.py
    update

--- main.py
+++ main.py
@@ -30,48 +30,14 @@ def serialize_f32(vector: List[float]) -> bytes:
     return struct.pack("%sf" % len(vector), *vector)
 
 
-db = sqlite3.connect(":memory:")
+db = sqlite3.connect("Ressources/vec.db")
 db.enable_load_extension(True)
 sqlite_vec.load(db)
 db.enable_load_extension(False)
 
-sqlite_version, vec_version = db.execute(
-    "select sqlite_version(), vec_version()"
-).fetchone()
-print(f"sqlite_version={sqlite_version}, vec_version={vec_version}")
-
-items = [
-    (1, [0.1, 0.1, 0.1, 0.1]),
-    (2, [0.2, 0.2, 0.2, 0.2]),
-    (3, [0.3, 0.3, 0.3, 0.3]),
-    (4, [0.4, 0.4, 0.4, 0.4]),
-    (5, [0.5, 0.5, 0.5, 0.5]),
-]
-query = [0.3, 0.3, 0.3, 0.3]
-
-db.execute("CREATE VIRTUAL TABLE vec_items USING vec0(embedding float[4])")
-
 with db:
-    for item in items:
-        db.execute(
-            "INSERT INTO vec_items(rowid, embedding) VALUES (?, ?)",
-            [item[0], serialize_f32(item[1])],
-        )
-
-rows = db.execute(
-    """
-      SELECT
-        rowid,
-        distance
-      FROM vec_items
-      WHERE embedding MATCH ?
-      ORDER BY distance
-      LIMIT 3
-    """,
-    [serialize_f32(query)],
-).fetchall()
-
-#print(rows)
+    result = db.execute("SELECT id, text FROM youtube LIMIT 10").fetchall()
+result
 
 st.header('testing')
-st.write(rows)
+st.write(result)
