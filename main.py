524fea0632df4e91823212992ee459b3001bf553
doyley91 <baurpas@gmail.com>
2024-12-24 13:52:27 +0100

    Update main.py
    update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import pandas as pd
 import numpy as np
+import random
 import streamlit as st
 import streamlit.components.v1 as components
 import sqlite3
